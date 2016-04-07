faa771ed9623948f59ed79d4c532d95e9e2da132
Jack Gardner <stonefeet@gmail.com>
2016-04-07 12:55:47 +0100

    Big server fixes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+var cors = require('cors');
 var app = express();
 
 // uncomment after placing your favicon in /public
@@ -12,6 +12,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors());
 
 module.exports = app;
