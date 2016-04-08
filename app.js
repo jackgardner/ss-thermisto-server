07aa20d8683c8cd2e81caa11cd7984963e648a24
calum <calumgardner23@gmail.com>
2016-04-08 22:05:13 +0100

    Added authentication

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var cors = require('cors');
-var app = express();
+var express         = require('express');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var jwt             = require('jsonwebtoken');
+var cors            = require('cors');
+var app             = express();
+const server_params = require('./config/server.json');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -13,5 +14,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
+// Temporary stuff
+
+var accounts = [
+  {
+    _id:   1234,
+    email: "account1@accounts.com",
+    password: "pass1"
+  },
+  {
+    _id:   1235,
+    email: "account2@accounts.com",
+    password: "pass2"
+  }
+]
+var jwtSecret = "ApplesAndOranges";
+
+app.post('/login', function(req, res){
+
+  /*
+  req.body = {
+    email: my@me.com,
+    password: Something
+  }
+  */
+
+  if (!req.body.email || !req.body.password) return;
+
+  var sent_token = false;
+  // Gotta be a better to do this
+  for (i = 0; i < accounts.length; i++) {
+    if (accounts[i].email    === req.body.email &&
+        accounts[i].password === req.body.password){
+
+          // Issue a token here.
+          var token = jwt.sign(accounts[i], server_params.token_secret, { expiresInMinutes: 60*5 });
+          res.json({token: token});
+          sent_token = true;
+        }
+  }
+  if (!sent_token) {
+    res.json({message: "Invalid Credentials!"});
+  }
+})
 
 module.exports = app;
--- socket.js
+++ socket.js
@@ -1,5 +1,7 @@
 // const config = require ('./config');
 const enums = require ('./lib/enums');
+const socketioJwt = require('socketio-jwt');
+const server_params = require('./config/server.json');
 
 const gameState = {
     state: enums.GAME_STATE.LOBBY,
@@ -15,6 +17,10 @@ module.exports = function (httpServer) {
     "use strict";
     var debug = require('debug')('ss-thermisto:server');
     var io = require('socket.io')(httpServer);
+    io.set('authorization', socketioJwt.authorize({
+      secret: server_params.token_secret,
+      handshake: true
+    }));
     var game = require('./lib/game')(io);
     return io;
 };
