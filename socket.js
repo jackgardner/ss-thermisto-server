4f5d3d27ed34bab6aa576eb9c6c463eeed0163a9
calum <calumgardner23@gmail.com>
2016-04-11 23:07:18 +0100

    updates

--- socket.js
+++ socket.js
@@ -1,25 +1,19 @@
 // const config = require ('./config');
 const enums = require ('./lib/enums');
-
+const gameStore = require('./lib/gameStore');
 
 const gameState = {
     state: enums.GAME_STATE.LOBBY,
     players: []
 };
-
-
-
-
 // TODO Setup game on first join
 
 module.exports = function (httpServer) {
     "use strict";
     var debug = require('debug')('ss-thermisto:server');
     var io = require('socket.io')(httpServer);
-    /*io.set('authorization', socketioJwt.authorize({
-      secret: server_params.token_secret,
-      handshake: true
-    }));*/
-    var game = require('./lib/game')(io);
-    return io;
+    gameStore.forEach( function (item) {
+      var nsp = io.of(`/${item._id}`);
+      var game = require('./lib/game')(nsp);
+    });
 };
